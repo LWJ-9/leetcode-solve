f37385a8594f2d8ba02879333dbbc0c5766e6496
LWJ-9 <lwj9@outlook.com>
2022-05-23 00:30:33 +0900

    add 167

--- 283. Move Zeroes.js
+++ 283. Move Zeroes.js
@@ -1,39 +1,40 @@
-// 283. Move Zeroes
-// Easy
+/* 
+283. Move Zeroes
+Easy
 
 
-// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
-// Note that you must do this in-place without making a copy of the array.
-
-
-
-// Example 1:
-
-// Input: nums = [0,1,0,3,12]
-// Output: [1,3,12,0,0]
-// Example 2:
-
-// Input: nums = [0]
-// Output: [0]
+Note that you must do this in-place without making a copy of the array.
 
 
-// Constraints:
 
-// 1 <= nums.length <= 104
-// -231 <= nums[i] <= 231 - 1
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
 
 
-// Follow up: Could you minimize the total number of operations done?
-// Accepted
-// 1,674,116
-// Submissions
-// 2,756,784
+Constraints:
 
-// In-place means we should not be allocating any space for extra array. But we are allowed to modify the existing array. However, as a first step, try coming up with a solution that makes use of additional space. For this problem as well, first apply the idea discussed using an additional array and the in-place solution will pop up eventually.
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 - 1
 
-// A two-pointer approach could be helpful here. The idea would be to have one pointer for iterating the array and another pointer that just works on the non-zero elements of the array.
 
+Follow up: Could you minimize the total number of operations done?
+Accepted
+1,674,116
+Submissions
+2,756,784
+
+In-place means we should not be allocating any space for extra array. But we are allowed to modify the existing array. However, as a first step, try coming up with a solution that makes use of additional space. For this problem as well, first apply the idea discussed using an additional array and the in-place solution will pop up eventually.
+
+A two-pointer approach could be helpful here. The idea would be to have one pointer for iterating the array and another pointer that just works on the non-zero elements of the array.
+ */
 
 /**
  * @param {number[]} nums
@@ -65,3 +66,12 @@ var moveZeroes = function (nums) {
 
 //[0,1,0,3,12]
 //console.log(moveZeroes([0, 1, 0, 3, 12]));
+
+/* 
+Success
+Details 
+Runtime: 90 ms, faster than 91.13% of JavaScript online submissions for Move Zeroes.
+Memory Usage: 46.6 MB, less than 63.03% of JavaScript online submissions for Move Zeroes.
+Next challenges:
+Remove Element
+ */
